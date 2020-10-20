ff5d562a945c5469376909878145c950f9f61f5a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-20 23:29:29 +0200

    Place body-parser and test get endpoint

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true} ));
 
-app.get('/quotes', (req, res) => {
+app.post('/quotes', (req, res) => {
   console.log('Hellooooooooooooooooo!')
+  console.log(req.body)
+  //return response.status(401).send({ "message": "Veillez vous identifier"});
+  //response.send({"pid":request.body.pid});
+  res.send(req.body);
 })
-//express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
